fb82df2088d9f0278c436c0ff79fc528b8468201
MEBonham <programmer@mckaybonham.com>
2018-11-26 11:58:39 -0700

    Followed along with lecture

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require("bcryptjs");
 
 const db = require('./database/dbConfig.js');
 
@@ -8,6 +9,39 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+server.post("/api/register", (req, res) => {
+  const creds = req.body;
+
+  const hash = bcrypt.hashSync(creds.password, 14); // rounds is 2^14
+
+  creds.password = hash;
+
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => json(err));
+});
+
+server.post("/api/login", (req, res) => {
+  const creds = req.body;
+
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        // Passwords match and user exists by that username;
+        // Do whatever when login is successful.
+        res.status(200).json({ message: "Welcome!" });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(err => res.json(err));
+});
+
 server.get('/', (req, res) => {
   res.send('Its Alive!');
 });
@@ -15,7 +49,7 @@ server.get('/', (req, res) => {
 // protect this route, only authenticated users should see it
 server.get('/api/users', (req, res) => {
   db('users')
-    .select('id', 'username')
+    .select('id', 'username', 'password')
     .then(users => {
       res.json(users);
     })
